c0fb292a341e60b9a9fa17b669775babe9cc9489
sanshiqi <1903943257@qq.com>
2025-08-13 16:56:25 +0800

    初始化项目结构 1.添加依赖和配置 2.提供基础代码（自定义异常， 请求相应包装类） 3.提供基础接口（健康检查接口）
    用户模块后端
    1. 开发代码生成器
    2. 开发核心功能，比如用户注册、用户登录、获取登录用户信息，用户注销等
    3. 全局权限校验AOP
    4. 开发管理员增删改查接口
    5. 自定义配置解决精度丢失问题

---
+++ src/main/java/com/jiaojiao/yuaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jiaojiao.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.jiaojiao.yuaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author jiaojiao
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
