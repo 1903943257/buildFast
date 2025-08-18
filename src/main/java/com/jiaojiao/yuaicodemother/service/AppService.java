4321b9eaf25e69f7583f756c9b69128afa729376
sanshiqi <1903943257@qq.com>
2025-08-18 15:49:08 +0800

    应用模块 基本功能已实现，测试通过

--- src/main/java/com/jiaojiao/yuaicodemother/service/AppService.java
+++ src/main/java/com/jiaojiao/yuaicodemother/service/AppService.java
@@ -1,7 +1,12 @@
 package com.jiaojiao.yuaicodemother.service;
 
+import com.jiaojiao.yuaicodemother.model.dto.app.AppQueryRequest;
+import com.jiaojiao.yuaicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.jiaojiao.yuaicodemother.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,28 @@ import com.jiaojiao.yuaicodemother.model.entity.App;
  * @author jiaojiao
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+
 
 }
